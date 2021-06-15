3d9d13364f1f0f1c0bdd5fe9566c2571777c2e69
nmtarr <nmtarr@ncsu.edu>
2021-06-15 14:16:29 -0400

    Added function

--- nate/nate.py
+++ nate/nate.py
@@ -96,3 +96,44 @@ def MakeRemap(mapUnitCodes, reclassValue):
         o.append(reclassValue)
         remap.append(o)
     return remap
+
+# Formula for longitude length
+def nominal_precisions(longitude, latitude, produce):
+    '''
+    Calculates the nominal precisions based on an WGS84 coordinates.
+    Method is based on information from wikipedia page on latitude and posts
+    at https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude
+    https://wiki.openstreetmap.org/wiki/Precision_of_coordinates
+
+    PARAMETERS
+    ----------
+    latitude : decimal degrees (EPSG:4326) latitude as string.
+    longitude : decimal degrees (EPSG:4326) longitude as string.
+    produce : 'longitude', 'latitude', or 'both'
+
+    RETURNS
+    -------
+    x : uncertainty in longitude (meters) as float.
+    y : uncertianty in latitude (meters) as float.
+
+    EXAMPLE
+    -------
+    x, y = nominal_precisions("-93.455", "26.3455", produce="both")
+    '''
+    lat = latitude.split(".")
+    long = longitude.split(".")
+
+    # Longitude
+    digitsX = {2: 100, 3: 1000, 4: 10000, 5: 100000}
+    x =(111321 * np.cos(float(latitude) * np.pi/180))/digitsX[len(long[1])] # decimal gets moved based on digits.
+
+    # Latitude
+    digitsY = {2: 1111.2, 3: 111.1, 4: 11.1, 5: 1.1} # Lookup for latitude precision
+    y = digitsY[len(lat[1])]
+
+    if produce == "both":
+        return x, y
+    if produce == "longitude":
+        return x
+    if produce == "latitude":
+        return y
